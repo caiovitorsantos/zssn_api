bf75d7907d030dc1db1e002e64d9988f90bfb24c
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-03-01 22:52:39 -0300

    Create comparison function to check if users are exchanging goods with the same score

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ApplicationRecord
   belongs_to :user
 
   enum kind: [:water, :food, :medicine, :ammunition]
-  points = [water: 4, food: 3, medicine: 2, ammunition: 1]
+  @@points = [ 4,  3,  2, 1]
 
   validates_presence_of :user_id, :kind
   validates_numericality_of :amount, greater_than_or_equal_to: 0, message: "Amount cannot be negative"
@@ -21,4 +21,45 @@ class Inventory < ApplicationRecord
   	self.amount -= amount_inc
   	self.save
   end
+
+  def self.equality(data_origin, data_destiny)
+    points_origin = points_destiny = 0
+      
+
+    binding.pry
+    data_origin[:items].each do |item|
+        points_origin += item[:amount] * @@points[item[:kind]]
+    end    
+
+    data_destiny[:items].each do |item|
+        points_destiny += item[:amount] * @@points[item[:kind]]
+    end    
+    
+    puts "Origin: #{points_origin} - Destiny: #{points_destiny}"
+
+    points_origin == points_destiny    
+  end
+
+
 end
+=begin
+{
+  "origin":
+  {
+    "user_id":1,
+    "items":[
+      { "kind":0, "amount":3 },
+      { "kind":2, "amount":1 }
+    ]
+  },
+  "destiny":
+  {
+    "user_id":2,
+    "items":[
+      { "kind":0, "amount":3 },
+      { "kind":2, "amount":1 }
+    ]
+  }
+  
+}
+=end
