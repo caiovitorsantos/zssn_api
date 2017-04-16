b558919547f07778fd7815036fe61250e70d1c81
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-04-16 18:35:17 -0300

    Fix the equality function, including infected user validations, and make exchage function

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -39,10 +39,16 @@ class Inventory < ApplicationRecord
   	self.save
   end
 
-  # 
+  # Verifica se os itens a serem trocados tem a mesmma quantidade de pontos, para uma troca igual 
   def self.equality(data_origin, data_destiny)
-    points_origin = points_destiny = 0
+    user_origin = User.find(data_origin[:user_id])
+    user_destiny = User.find(data_destiny[:user_id])
       
+    user_origin.errors.add(:base, "The user is infected") and return false unless user_origin.healthy?
+    user_destiny.errors.add(:base, "The user is infected") and return false unless user_destiny.healthy?
+
+    points_origin = points_destiny = 0
+
     data_origin[:items].each do |item|
         points_origin += item[:amount] * @@points[item[:kind]]
     end    
@@ -52,6 +58,36 @@ class Inventory < ApplicationRecord
     end    
 
     points_origin == points_destiny    
+  end
+
+  # Realiza a troca bilateral do items entre dois usuário
+  # Da forma que estruturado esse método é possível realizar uma troca unilateral passando o terceiro paramentro com
+  # o valor true 
+  def self.exchange(data_origin, data_destiny, recursed = false)
+    data_origin[:items].each do |item|  
+      inv_sub = Inventory.where(user_id: data_origin[:user_id], kind: item[:kind]).first
+
+      if inv_sub.nil?
+        inv_sub.errors.add(:base, "The inventory doesn't exist to remove items") and return false
+      end
+
+      unless inv_sub.user.healthy?
+        inv_sub.errors.add(:user_id, "The user is infected") and return false
+      end
+
+      inv_add = Inventory.where(user_id: data_destiny[:user_id], kind: item[:kind]).first_or_create do |user|
+        user.user_id = data_destiny[:user_id]
+        user.kind = item[:kind]
+        user.amount ||= 0
+      end
+      inv_add.amount += item[:amount]
+      inv_sub.amount -= item[:amount]
+
+      inv_add.save
+      inv_sub.save
+    end
+
+    self.exchange(data_destiny, data_origin, true) unless recursed
   end
 
 end
