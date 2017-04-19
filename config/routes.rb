956061797d487a8fe273f40760199447a5e474ef
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-04-19 01:17:29 -0300

    Refactor the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
+	get 'report', to: 'users#reports'
 	resources :users do
 		member do
-			get 'report', to: 'users#report'
+			get 'complaint', to: 'users#complaint'
+			get 'set_location', to: 'users#set_location'
 			get 'inventory', to: 'users#inventory'
 		end
 	end
 
-
 	post 'exchange', to: 'inventories#exchange'
+	# As duas rotas abixo estão fora do resources pois recebem parametros "id" compostos {:user_id e :kind} 
+	get 'show', to: 'inventories#show'
+	get 'index', to: 'inventories#index'
 	resources :inventories do
 		member do
 			post 'add', to: 'inventories#add'
