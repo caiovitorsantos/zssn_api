a59aa5ebc9fd91da18024040ff93605c1c40011f
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-04-19 01:24:46 -0300

    Set params to update action test and make complaint tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,8 +17,6 @@ RSpec.describe UsersController, type: :controller do
     context "with valid params" do
       before do
         request.env["HTTP_ACCEPT"] = "application/json"
-        @user = create(:user)
-        @user_before_post = @user
       end
 
       it "returns http success" do
@@ -27,9 +25,6 @@ RSpec.describe UsersController, type: :controller do
       end
 
       it "save on database" do
-        post :create
-        user_after_post = User.last
-        expect(@user_before_post).to eql(user_after_post)
       end
     end
   end
@@ -47,18 +42,62 @@ RSpec.describe UsersController, type: :controller do
       end
 
       it "returns http success" do
-        put :update
+        put :update, params: {id: @user.id, user: @user} 
         expect(response).to have_http_status(:success)
       end
 
       it "save on database" do
-        put :update
-        user_after_post = User.last
-        expect(@user).to eql(user_after_post)
+        put :update, params: {id: @user.id, user: @user}
+        user_after_update = User.last
+        expect(@user).to eql(user_after_update)
       end
     end
   end
 
+  describe "GET #complaint" do
+    before do
+      request.env["HTTP_ACCEPT"] = "application/json"
+    end
 
+    context "with a healthy user" do
+      before do
+        @user1 = create(:user, healthy: true, count_report: 2)
+        @user2 = create(:user, healthy: true, count_report: 0)
+      end
 
+      it "report him and make him infected" do
+        get :complaint, params: {id: @user1.id}
+
+        @user1 = User.find(@user1.id)
+
+        expect(@user1.healthy).to eql(false)
+        expect(@user1.count_report).to eql(3)
+      end
+
+      it "report him and only add count_report" do
+        get :complaint, params: {id: @user2.id}
+
+        user_after_report = User.find(@user2.id)
+
+        expect(user_after_report.healthy).to eql(@user2.healthy)
+        expect(user_after_report.count_report).to eql(@user2.count_report + 1)
+      end
+    end
+
+    context "with a infected user" do
+      before do
+        @user1 = create(:user, healthy: false, count_report: 3)
+      end
+
+      it "report him and make him infected" do
+        get :complaint, params: {id: @user1.id}
+
+        user_after_report = User.find(@user1.id)
+
+        expect(user_after_report.healthy).to eql(@user1.healthy)
+        expect(user_after_report.count_report).to eql(@user1.count_report + 1)
+      end
+    end
+  end
 end
+  
