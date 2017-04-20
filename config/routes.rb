b6178a05595f4e9923334420454be4218777e824
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-04-19 22:28:15 -0300

    Fix inventory routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
-	get 'report', to: 'users#reports'
+	get 'reports', to: 'users#reports'
 	resources :users do
 		member do
 			get 'complaint', to: 'users#complaint'
-			get 'set_location', to: 'users#set_location'
+			put 'set_location', to: 'users#set_location'
 			get 'inventory', to: 'users#inventory'
 		end
 	end
 
 	post 'exchange', to: 'inventories#exchange'
 	# As duas rotas abixo estão fora do resources pois recebem parametros "id" compostos {:user_id e :kind} 
-	get 'show', to: 'inventories#show'
 	get 'index', to: 'inventories#index'
 	resources :inventories do
 		member do
